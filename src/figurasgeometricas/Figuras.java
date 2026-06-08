5ac3a71223e0cfe40a07469e1f6112fca4fdb717
DarioRodriguez09 <darioroel22@gmail.com>
2026-06-08 16:57:18 -0600

    Se crea clases Ciculo y Figuras
    Se realizaron las clases y calculos en estas clases

---
+++ src/figurasgeometricas/Figuras.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package figurasgeometricas;
+
+/**
+ *
+ * @author Dario R
+ */
+public abstract class Figuras {
+    protected double area;
+    protected double perimetro;
+
+    public double getArea() {
+        return area;
+    }
+    public double getPerimetro() {
+        return perimetro;
+    }
+    public abstract double calcularPerimetro();
+    public abstract double calcularArea();
+    
+    
+    
+}
